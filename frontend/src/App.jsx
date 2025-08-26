59ad0fa1277f346c7f010c1268c4ccc25ca18c93
D DARSHAN <darshand4802@gmail.com>
2025-08-26 18:52:00 +0530

    Initial commit with project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Landingpage from './pages/Landingpage'
+
+function App() {
+  return(
+<Landingpage/>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Landingpage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Landingpage = () => {
+  return (
+    <div>
+      <h1>Digital Civix Engagement Landing Page .!!</h1>
+    </div>
+  )
+}
+
+export default Landingpage;
